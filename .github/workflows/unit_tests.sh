8ad6353d95817b88606908d5a6abb69ac0863dea
TaehyeonPark <devtaehyeon@gmail.com>
2024-12-09 23:19:20 +0900

    Add: Github Actions

--- .github/workflows/unit_tests.sh
+++ .github/workflows/unit_tests.sh
@@ -1,6 +1,22 @@
+# #!/bin/bash
+# set -e
+# TEST_DIR="tests"
+
+# echo "Running unit tests..."
+# pytest --junitxml=test-results/results.xml $TEST_DIR
+# echo "All tests passed!"
 #!/bin/bash
 set -e
 TEST_DIR="tests"
+
+echo "Current working directory: $(pwd)"
+
+cd "$(dirname "$0")/../../"
+
+if [ ! -f "./presets.json" ]; then
+    echo "Error: presets.json not found!"
+    exit 1
+fi
 
 echo "Running unit tests..."
 pytest --junitxml=test-results/results.xml $TEST_DIR
