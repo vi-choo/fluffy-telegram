615cb52899f0fa9780ed2d33d13a38f5469bd2e8
TaehyeonPark <devtaehyeon@gmail.com>
2024-12-09 23:59:55 +0900

    Add: Github Actions

--- .github/workflows/mutation_tests.sh
+++ .github/workflows/mutation_tests.sh
@@ -5,6 +5,6 @@ echo "Current working directory: $(pwd)"
 
 cd "$(dirname "$0")/../../"
 
-echo "Running unit tests..."
+echo "Running mutation tests..."
 pytest --junitxml=test-results/results.xml $TEST_DIR -s
 echo "All tests passed!"
